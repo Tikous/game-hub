ebaad40ac57f6a45fc11bd9aac4e145e649dd5cb
Tikous <1120531060@qq.com>
2024-04-22 19:17:17 +0800

    feat: deploy

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import { HStack, Image } from '@chakra-ui/react'
+import logo from '../assets/logo.webp'
+import ColorModSwitch from './ColorModSwitch'
+import SearchInput from './SearchInput'
+
+interface Props {
+  onSearch: (searchText: string) => void
+}
+
+const NavBar = ({ onSearch }: Props) => {
+  return (
+    // horizontal stack
+    <HStack padding='10px'>
+      <Image src={logo} boxSize='60px' />
+      <SearchInput onSearch={onSearch} />
+      <ColorModSwitch />
+    </HStack>
+  )
+}
+
+export default NavBar
